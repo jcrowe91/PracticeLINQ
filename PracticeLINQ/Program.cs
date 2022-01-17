692bb9fb808a45faf11b06722d9983f1b4551a1b
jcrowe91 <jcrowe9119@gmail.com>
2022-01-16 20:06:44 -0600

    Add project files.

---
+++ PracticeLINQ/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PracticeLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Metal Gear Solid, Resident Evil, Elder Scrolls, Super Smash Bros
+
+
+        }
+    }
+}
